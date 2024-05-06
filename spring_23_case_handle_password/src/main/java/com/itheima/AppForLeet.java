68061c9bcd18c8663e14d2665d6d77beb4569784
zllwhu <2023202210120@whu.edu.cn>
2024-05-06 11:21:41 +0800

    20240506: 1. Debug for leetcode.

--- spring_23_case_handle_password/src/main/java/com/itheima/AppForLeet.java
+++ spring_23_case_handle_password/src/main/java/com/itheima/AppForLeet.java
@@ -1,5 +1,8 @@
 package com.itheima;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class AppForLeet {
     public static void main(String[] args) {
         int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
@@ -7,6 +10,8 @@ public class AppForLeet {
         for (int i = 0; i < lens; i++) {
             System.out.println(nums[i]);
         }
+        int[][] mat = {{3,3,1,1},{2,2,1,2},{1,1,1,2}};
+        diagonalSort(mat);
     }
 
     public static int removeElement(int[] nums, int val) {
@@ -18,5 +23,18 @@ public class AppForLeet {
             }
         }
         return i;
+    }
+
+    public static int[][] diagonalSort(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int key = i - j;
+                map.computeIfAbsent(key, k -> new ArrayList<>()).add(mat[i][j]);
+            }
+        }
+        return null;
     }
 }
